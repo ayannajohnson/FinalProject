e6bfada5e9bc23e9646e75086b3155bca34b8d0f
syniah <syniahfeatherstone@gmail.com>
2016-07-21 15:03:20 -0700

    about the creators page, map

--- js/aboutme.js
+++ js/aboutme.js
@@ -6,8 +6,8 @@ function  mouseover1 () {
 
 function mouseout1 () {
   var image1 = document.getElementById("syniah");
-  image1.style.width = "200px";
-  image1.style.height = "150px";
+  image1.style.width = "300px";
+  image1.style.height = "250px";
 }
 
 function  mouseover2 () {
@@ -18,8 +18,8 @@ function  mouseover2 () {
 
 function mouseout2 () {
   var image2 = document.getElementById("jassy");
-  image2.style.width = "200px";
-  image2.style.height = "150px";
+  image2.style.width = "300px";
+  image2.style.height = "250px";
 }
 
 function  mouseover3 () {
@@ -30,8 +30,8 @@ function  mouseover3 () {
 
 function mouseout3 () {
   var image3= document.getElementById("ayanna");
-  image3.style.width = "200px";
-  image3.style.height = "150px";
+  image3.style.width = "300px";
+  image3.style.height = "250px";
 }
 
 function  mouseover4 () {
@@ -42,6 +42,6 @@ function  mouseover4 () {
 
 function mouseout4 () {
   var image4 = document.getElementById("rayvon");
-  image4.style.width = "200px";
-  image4.style.height = "150px";
+  image4.style.width = "300px";
+  image4.style.height = "250px";
 }

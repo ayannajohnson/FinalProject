7db5d13cb210d8f7f9371fc6272d8f064a98dd3c
Ravyn <rhavenn12@gmail.com>
2016-07-26 10:51:09 -0700

    Finished Newspage and About the Creators

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,7 +1,7 @@
 function  mouseover1 () {
   var image1 = document.getElementById("syniah");
-  image1.style.width = "500px";
-  image1.style.height = "400px";
+  image1.style.width = "350px";
+  image1.style.height = "300px";
 }
 
 function mouseout1 () {
@@ -12,8 +12,8 @@ function mouseout1 () {
 
 function  mouseover2 () {
   var image2 = document.getElementById("jassy");
-  image2.style.width = "500px";
-  image2.style.height = "400px";
+  image2.style.width = "350px";
+  image2.style.height = "300px";
 }
 
 function mouseout2 () {
@@ -24,8 +24,8 @@ function mouseout2 () {
 
 function  mouseover3 () {
   var image3 = document.getElementById("ayanna");
-  image3.style.width = "500px";
-  image3.style.height = "400px";
+  image3.style.width = "350px";
+  image3.style.height = "300px";
 }
 
 function mouseout3 () {
@@ -36,8 +36,8 @@ function mouseout3 () {
 
 function  mouseover4 () {
   var image4 = document.getElementById("rayvon");
-  image4.style.width = "500px";
-  image4.style.height = "400px";
+  image4.style.width = "350px";
+  image4.style.height = "300px";
 }
 
 function mouseout4 () {

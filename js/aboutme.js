ba275224a439717de09775692a11402a8d012891
Ravyn <rhavenn12@gmail.com>
2016-07-27 11:02:19 -0700

    fixed news page

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,47 +1,47 @@
 function  mouseover1 () {
   var image1 = document.getElementById("syniah");
-  image1.style.width = "350px";
-  image1.style.height = "300px";
+  image1.style.width = "300px";
+  image1.style.height = "350px";
 }
 
 function mouseout1 () {
   var image1 = document.getElementById("syniah");
-  image1.style.width = "200px";
-  image1.style.height = "150px";
+  image1.style.width = "300px";
+  image1.style.height = "250x";
 }
 
 function  mouseover2 () {
   var image2 = document.getElementById("jassy");
-  image2.style.width = "350px";
-  image2.style.height = "300px";
+  image2.style.width = "300px";
+  image2.style.height = "350px";
 }
 
 function mouseout2 () {
   var image2 = document.getElementById("jassy");
-  image2.style.width = "200px";
-  image2.style.height = "150px";
+  image2.style.width = "300px";
+  image2.style.height = "250px";
 }
 
 function  mouseover3 () {
   var image3 = document.getElementById("ayanna");
-  image3.style.width = "350px";
-  image3.style.height = "300px";
+  image3.style.width = "300px";
+  image3.style.height = "350px";
 }
 
 function mouseout3 () {
   var image3= document.getElementById("ayanna");
-  image3.style.width = "200px";
-  image3.style.height = "150px";
+  image3.style.width = "300px";
+  image3.style.height = "250px";
 }
 
 function  mouseover4 () {
   var image4 = document.getElementById("rayvon");
-  image4.style.width = "350px";
-  image4.style.height = "300px";
+  image4.style.width = "300px";
+  image4.style.height = "350px";
 }
 
 function mouseout4 () {
   var image4 = document.getElementById("rayvon");
-  image4.style.width = "200px";
-  image4.style.height = "150px";
+  image4.style.width = "300px";
+  image4.style.height = "250px";
 }
